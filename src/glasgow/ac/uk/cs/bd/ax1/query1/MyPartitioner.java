db30019fe07c888b403f320073891509689a2a56
Vincent Fung <vincentfung13@users.noreply.github.com>
2016-02-05 20:33:24 +0000

    Initial commit

---
+++ src/glasgow/ac/uk/cs/bd/ax1/query1/MyPartitioner.java
@@ -0,0 +1,17 @@
+package glasgow.ac.uk.cs.bd.ax1.query1;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class MyPartitioner extends Partitioner<Text, IntWritable>{
+
+	@Override
+	public int getPartition(Text key, IntWritable value, int numPartitions) {
+		int c = Character.toLowerCase(key.toString().charAt(0)); 
+		if (c < 'a' || c > 'z')
+			return numPartitions - 1;
+		return (int)Math.floor((float)(numPartitions - 2) * (c - 'a')/('z' - 'a'));
+	}
+
+}
---
+++ src/glasgow/ac/uk/cs/bd/ax1/query1/QueryOneReducer.java
@@ -0,0 +1,25 @@
+package glasgow.ac.uk.cs.bd.ax1.query1;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class QueryOneReducer extends Reducer<Text, Text, Text, Text> {
+	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+		int count = 0;
+		for(@SuppressWarnings("unused") Text value: values) {
+			count++;
+		}
+			
+		String ReducerValues = Integer.toString(count) + " ";
+			
+		for(Text value : values){
+			ReducerValues = ReducerValues + value.toString() + " ";
+		}
+			
+		Text results = new Text(ReducerValues);
+			
+		context.write(key, results);   
+	}  
+}  
